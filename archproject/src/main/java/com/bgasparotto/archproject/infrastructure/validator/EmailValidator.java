77c578d638ba1c58d84330e187fb31238a92030b
Bruno Gasparotto <bruno.m.gasparotto@gmail.com>
2015-05-10 19:17:52 -0300

    Added an e-mail validator which relies on RFC 2822 specification.

---
+++ archproject/src/main/java/com/bgasparotto/archproject/infrastructure/validator/EmailValidator.java
@@ -0,0 +1,41 @@
+package com.bgasparotto.archproject.infrastructure.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * E-mail validator using {@code regex} according to <a
+ * href="https://www.ietf.org/rfc/RFC_2822.txt">RFC 2822</a> specification and
+ * no external libraries.
+ * 
+ * @author Bruno Gasparotto
+ *
+ */
+public class EmailValidator {
+	private static final Pattern RFC_2822;
+
+	static {
+		String regex = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+				+ "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:"
+				+ "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+"
+				+ "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+
+		RFC_2822 = Pattern.compile(regex);
+	}
+
+	/**
+	 * Validate an email address.
+	 * 
+	 * @param email
+	 *            The email to be validated
+	 * @return {@code true} if the given email is valid, {@code false} otherwise
+	 */
+	public boolean validate(String email) {
+		if ((email == null) || (email.isEmpty())) {
+			return false;
+		}
+
+		Matcher matcher = RFC_2822.matcher(email);
+		return matcher.matches();
+	}
+}
---
+++ archproject/src/main/java/com/bgasparotto/archproject/infrastructure/validator/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing validator classes of {@code infrastructure} layer.
+ */
+package com.bgasparotto.archproject.infrastructure.validator;
