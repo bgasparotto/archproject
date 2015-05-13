4d489be282e1c0a12f118e0bd4777999a33bac3e
Bruno Gasparotto <bruno.m.gasparotto@gmail.com>
2015-05-13 12:28:38 -0300

    Added missing package-info.

---
+++ archproject/src/main/java/com/bgasparotto/archproject/model/identity/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing interfaces that can be used as an identifier for an
+ * object, for example, a {@code Long} field named {@code id} that uniquely
+ * distinguish one persisted entity from another of the same type.
+ */
+package com.bgasparotto.archproject.model.identity;
