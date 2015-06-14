5726356ba293311bf55c1d6b55465048509e2f5b
Bruno Gasparotto <bruno.m.gasparotto@gmail.com>
2015-06-14 02:19:40 -0300

    Added role table and a user_role relationship.

--- archproject.sql
+++ archproject.sql
@@ -40,4 +40,39 @@ CREATE TABLE security.user(
 
 -- ddl-end --
 
+-- object: security.role | type: TABLE --
+CREATE TABLE security.role(
+	id_role serial NOT NULL,
+	name character varying(16) NOT NULL,
+	CONSTRAINT pk_role_id_role PRIMARY KEY (id_role),
+	CONSTRAINT uq_role_name UNIQUE (name)
+);
+-- ddl-end --
+
+-- ddl-end --
+
+-- object: security.user_role | type: TABLE --
+CREATE TABLE security.user_role(
+	id_user integer,
+	id_role integer,
+	CONSTRAINT pk_user_role PRIMARY KEY (id_user,id_role)
+);
+-- ddl-end --
+
+-- ddl-end --
+
+-- object: fk_user_role_user | type: CONSTRAINT --
+ALTER TABLE security.user_role ADD CONSTRAINT fk_user_role_user FOREIGN KEY (id_user)
+REFERENCES security.user (id_user) MATCH FULL
+ON DELETE RESTRICT ON UPDATE CASCADE NOT DEFERRABLE;
+-- ddl-end --
+
+
+-- object: fk_user_role_role | type: CONSTRAINT --
+ALTER TABLE security.user_role ADD CONSTRAINT fk_user_role_role FOREIGN KEY (id_role)
+REFERENCES security.role (id_role) MATCH FULL
+ON DELETE RESTRICT ON UPDATE CASCADE NOT DEFERRABLE;
+-- ddl-end --
+
+
 
