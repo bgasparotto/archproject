512a45a5308b16aa8fcb95278412d659b82fbde5
Bruno Gasparotto <bruno.m.gasparotto@gmail.com>
2015-06-23 14:33:52 -0300

    Refactor to extract interface from EmailValidator.

--- archproject/src/main/java/com/bgasparotto/archproject/infrastructure/validator/EmailValidator.java
+++ archproject/src/main/java/com/bgasparotto/archproject/infrastructure/validator/EmailValidator.java
@@ -1,32 +1,13 @@
 package com.bgasparotto.archproject.infrastructure.validator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
- * E-mail validator using {@code regex} according to <a
- * href="https://www.ietf.org/rfc/rfc2822.txt">RFC 2822</a> specification and
- * no external libraries.
+ * E-mail address validator.
  * 
  * @author Bruno Gasparotto
  *
  */
-public class EmailValidator {
-
-	/**
-	 * The {@code RFC_2822} regular expression pattern.
-	 */
-	public static final String RFC_2822_REGEXP = "^"
-			+ "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
-			+ "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:"
-			+ "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+"
-			+ "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
-
-	private static final Pattern RFC_2822;
-
-	static {
-		RFC_2822 = Pattern.compile(RFC_2822_REGEXP);
-	}
+@FunctionalInterface
+public interface EmailValidator {
 
 	/**
 	 * Validate an email address.
@@ -35,12 +16,5 @@ public class EmailValidator {
 	 *            The email to be validated
 	 * @return {@code true} if the given email is valid, {@code false} otherwise
 	 */
-	public boolean validate(String email) {
-		if ((email == null) || (email.isEmpty())) {
-			return false;
-		}
-
-		Matcher matcher = RFC_2822.matcher(email);
-		return matcher.matches();
-	}
+	boolean validate(String email);
 }
---
+++ archproject/src/main/java/com/bgasparotto/archproject/infrastructure/validator/Rfc2822EmailValidator.java
@@ -0,0 +1,40 @@
+package com.bgasparotto.archproject.infrastructure.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * E-mail validator using {@code regex} according to <a
+ * href="https://www.ietf.org/rfc/rfc2822.txt">RFC 2822</a> specification and no
+ * external libraries.
+ * 
+ * @author Bruno Gasparotto
+ *
+ */
+public class Rfc2822EmailValidator implements EmailValidator {
+
+	/**
+	 * The {@code RFC_2822} regular expression pattern.
+	 */
+	public static final String RFC_2822_REGEXP = "^"
+			+ "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+			+ "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:"
+			+ "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+"
+			+ "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+
+	private static final Pattern RFC_2822;
+
+	static {
+		RFC_2822 = Pattern.compile(RFC_2822_REGEXP);
+	}
+
+	@Override
+	public boolean validate(String email) {
+		if ((email == null) || (email.isEmpty())) {
+			return false;
+		}
+
+		Matcher matcher = RFC_2822.matcher(email);
+		return matcher.matches();
+	}
+}
