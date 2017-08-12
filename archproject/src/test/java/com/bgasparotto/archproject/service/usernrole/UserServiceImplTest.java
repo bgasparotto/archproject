635c877704d8324a90fa1c7ca59d02f53330d35d
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-08-11 22:30:57 -0300

    Added some unit tests for UserServiceImpl.

--- archproject/src/test/java/com/bgasparotto/archproject/service/usernrole/UserServiceImplTest.java
+++ archproject/src/test/java/com/bgasparotto/archproject/service/usernrole/UserServiceImplTest.java
@@ -3,16 +3,19 @@ package com.bgasparotto.archproject.service.usernrole;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import com.bgasparotto.archproject.model.Authentication;
 import com.bgasparotto.archproject.model.Credential;
+import com.bgasparotto.archproject.model.Login;
 import com.bgasparotto.archproject.model.Password;
 import com.bgasparotto.archproject.model.Registration;
 import com.bgasparotto.archproject.model.RolesGroup;
 import com.bgasparotto.archproject.model.User;
-import com.bgasparotto.archproject.model.Login;
 import com.bgasparotto.archproject.persistence.dao.UserDao;
 import com.bgasparotto.archproject.service.AbstractServiceTest;
-import com.bgasparotto.archproject.service.usernrole.UserServiceImpl;
 
 /**
  * Unit service tests for {@link UserServiceImpl}.
@@ -22,12 +25,21 @@ import com.bgasparotto.archproject.service.usernrole.UserServiceImpl;
  */
 public class UserServiceImplTest
 		extends AbstractServiceTest<User, UserServiceImpl, UserDao> {
+	private UserService userService;
+	private UserDao daoMock;
 
 	/**
 	 * Constructor.
 	 */
 	public UserServiceImplTest() {
 		super(UserServiceImpl.class, UserDao.class);
+	}
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		userService = (UserService) getService();
+		daoMock = getDaoMock();
 	}
 
 	@Override
@@ -49,7 +61,73 @@ public class UserServiceImplTest
 		return 8;
 	}
 	
-	public static void main(String[] args) {
-		System.out.println(UUID.randomUUID().toString().length());
+	@Test
+	public void shouldFindExistingUsername() throws Exception {
+		String username = "someuser";
+		Mockito.when(daoMock.findByUsername(username))
+				.thenReturn(getExpectedEntity());
+
+		User user = userService.findByUsername(username);
+		Assert.assertNotNull(user);
+	}
+	
+	@Test
+	public void shouldReturnNullWhenUsernameDoesntExist() throws Exception {
+		String username = "nonexistent";
+		Mockito.when(daoMock.findByUsername(username)).thenReturn(null);
+
+		User user = userService.findByUsername(username);
+		Assert.assertNull(user);
+	}
+	
+	@Test
+	public void shouldReturnNullWhenPassesNullUsername() throws Exception {
+		Mockito.when(daoMock.findByUsername(null)).thenReturn(null);
+		
+		User user = userService.findByUsername(null);
+		Assert.assertNull(user);
+	}
+	
+	@Test
+	public void shouldReturnNullWhenPassesEmptyUsername() throws Exception {
+		Mockito.when(daoMock.findByUsername("")).thenReturn(null);
+		
+		User user = userService.findByUsername("");
+		Assert.assertNull(user);
+	}
+	
+	@Test
+	public void shouldFindExistingEmail() throws Exception {
+		String email = "someuser@gmail.com";
+		Mockito.when(daoMock.findByEmail(email))
+				.thenReturn(getExpectedEntity());
+
+		User user = userService.findByEmail(email);
+		Assert.assertNotNull(user);
+	}
+	
+	@Test
+	public void shouldReturnNullWhenEmailDoesntExist() throws Exception {
+		String email = "nonexistent@gmail.com";
+		Mockito.when(daoMock.findByEmail(email)).thenReturn(null);
+
+		User user = userService.findByEmail(email);
+		Assert.assertNull(user);
+	}
+	
+	@Test
+	public void shouldReturnNullWhenPassesNullEmail() throws Exception {
+		Mockito.when(daoMock.findByEmail(null)).thenReturn(null);
+		
+		User user = userService.findByEmail(null);
+		Assert.assertNull(user);
+	}
+	
+	@Test
+	public void shouldReturnNullWhenPassesEmptyEmail() throws Exception {
+		Mockito.when(daoMock.findByEmail("")).thenReturn(null);
+		
+		User user = userService.findByEmail("");
+		Assert.assertNull(user);
 	}
 }
