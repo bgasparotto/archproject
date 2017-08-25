006ac223432d96751b09a302415238abb11de395
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-08-25 14:21:08 -0300

    Implemented an annotation for exception intercepting and logging.

---
+++ archproject/src/main/java/com/bgasparotto/archproject/infrastructure/interceptor/ExceptionLogging.java
@@ -0,0 +1,27 @@
+package com.bgasparotto.archproject.infrastructure.interceptor;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.interceptor.InterceptorBinding;
+
+/**
+ * Types or methods annotated with {@code ExceptionLogging} will log any
+ * thrown exceptions in the default {@code Logger} object, so logging strategies
+ * don't need to be mixed with logic and exception handling.
+ * 
+ * @author Bruno Gasparotto
+ *
+ */
+@Inherited
+@InterceptorBinding
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface ExceptionLogging {
+
+}
---
+++ archproject/src/main/java/com/bgasparotto/archproject/infrastructure/interceptor/ExceptionLoggingInterceptor.java
@@ -0,0 +1,54 @@
+package com.bgasparotto.archproject.infrastructure.interceptor;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+import org.slf4j.Logger;
+
+/**
+ * Logging interceptor.
+ * 
+ * @author Bruno Gasparotto
+ *
+ */
+@ExceptionLogging
+@Interceptor
+public class ExceptionLoggingInterceptor {
+	private Logger logger;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param logger
+	 *            The {@code Logger} to be used by this interceptor
+	 */
+	@Inject
+	public ExceptionLoggingInterceptor(Logger logger) {
+		this.logger = logger;
+	}
+
+	/**
+	 * Intercepts a method invocation to write error log if an exception is
+	 * throw during its execution.
+	 * 
+	 * @param joinPoint
+	 *            The join point of the method on the stack
+	 * @return The intercepted method result, or {@code null} if its return type
+	 *         is {@code void} or its part of an interceptor lifecycle
+	 * @throws Exception
+	 *             thrown by the invoked method after logging
+	 */
+	@AroundInvoke
+	public Object log(InvocationContext joinPoint) throws Exception {
+		try {
+			Object o = joinPoint.proceed();
+			return o;
+		} catch (Exception e) {
+			String message = e.getMessage();
+			logger.error(message, e);
+			throw e;
+		}
+	}
+}
