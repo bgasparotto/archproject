fe77334f1a2830b452bfb910680f8cde31de8fe7
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-08-26 10:13:00 -0300

    Updated the validation mail to send the parameterised validation link.

--- archproject/src/main/java/com/bgasparotto/archproject/service/mail/MailServiceImpl.java
+++ archproject/src/main/java/com/bgasparotto/archproject/service/mail/MailServiceImpl.java
@@ -5,15 +5,15 @@ import javax.inject.Inject;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-
+import com.bgasparotto.archproject.infrastructure.interceptor.ExceptionLogging;
 import com.bgasparotto.archproject.infrastructure.mail.MailSender;
 import com.bgasparotto.archproject.model.Authentication;
 import com.bgasparotto.archproject.model.Credential;
+import com.bgasparotto.archproject.model.Login;
 import com.bgasparotto.archproject.model.Registration;
 import com.bgasparotto.archproject.model.User;
-import com.bgasparotto.archproject.model.Login;
 import com.bgasparotto.archproject.model.mail.Mail;
 import com.bgasparotto.archproject.service.exception.ServiceException;
 
@@ -24,13 +24,14 @@ import com.bgasparotto.archproject.service.exception.ServiceException;
  *
  */
 @RequestScoped
+@ExceptionLogging
 public class MailServiceImpl implements MailService {
 
 	@Inject
 	private MailSender sender;
-
+	
 	@Inject
-	private Logger logger;
+	private HttpServletRequest request;
 
 	@Override
 	public void sendValidationEmail(User user) throws ServiceException {
@@ -43,15 +44,53 @@ public class MailServiceImpl implements MailService {
 		Registration registration = user.getRegistration();
 		String verificationCode = registration.getVerificationCode();
 		
-		String message = "Thanks for your registration " + username
-				+ ", here's your verification code: " + verificationCode
-				+ ". Don't worry, you don't need to use it yet.";
+		String message = buildVerificationMessage(username, verificationCode);
 
 		String from = "bgasparotto-archproject@gmail.com";
 		String subject = "Registration Details";
 		Mail mail = new Mail(from, email, subject, message);
 		
 		sendMail(mail);
+	}
+
+	/**
+	 * Builds the verification message to be send by e-mail.
+	 * 
+	 * @param username
+	 *            The username of the destination user
+	 * @param verificationCode
+	 *            The verification code of the user
+	 * @return The message to be sent to the user
+	 */
+	private String buildVerificationMessage(String username,
+			String verificationCode) {
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("Thanks for your registration ").append(username);
+		builder.append(", open the link below to validate your account: \n");
+		
+		String baseUrl = buildVerificationBaseUrl();
+		builder.append(baseUrl).append("/pages/public/validation.xhtml?");
+		builder.append("username=").append(username);
+		builder.append("&");
+		builder.append("verificationCode=").append(verificationCode);
+
+		return builder.toString();
+	}
+	
+	/**
+	 * Builds the base URL to be combined with the validation page and
+	 * parameters.
+	 * 
+	 * @return The base URL of the application.
+	 */
+	private String buildVerificationBaseUrl() {
+		String url = request.getRequestURL().toString();
+		String uri = request.getRequestURI();
+		String path = request.getContextPath();
+		
+		url = url.substring(0, url.length() - uri.length()) + path;
+		return url;
 	}
 
 	@Override
@@ -63,7 +102,6 @@ public class MailServiceImpl implements MailService {
 			sender.send(message);
 		} catch (MessagingException e) {
 			String message = "Failed to send an e-mail message.";
-			logger.error(message, e);
 			throw new ServiceException(message, e);
 		}
 	}
