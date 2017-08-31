253762f3356be89f29f6b55fcd0a9a24f2df9f8e
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-08-31 16:46:13 -0300

    Added element ids and names to the pages and created unit tests.

--- archproject/src/test/java/com/bgasparotto/archproject/web/security/LoginTest.java
+++ archproject/src/test/java/com/bgasparotto/archproject/web/security/LoginTest.java
@@ -2,6 +2,10 @@ package com.bgasparotto.archproject.web.security;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.bgasparotto.archproject.web.ArquillianTestCase;
 
@@ -11,10 +15,26 @@ public class LoginTest extends ArquillianTestCase {
 	protected String targetUrl() {
 		return "localhost:8080/archproject";
 	}
-
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		WebElement signInLink = driver.findElement(By.name("sign-in"));
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.elementToBeClickable(By.name("sign-in")));
+		signInLink.click();
+	}
+	
 	@Test
-	public void shouldRun() throws Exception {
-		System.out.println("LoginTest!");
-		Assert.assertTrue(true);
+	public void shouldFindLogInElements() {
+		WebElement usernameInput = driver.findElement(By.id("j_username"));
+		Assert.assertNotNull(usernameInput);
+		
+		WebElement passwordInput = driver.findElement(By.id("j_password"));
+		Assert.assertNotNull(passwordInput);
+		
+		WebElement loginButton = driver.findElement(By.name("loginButton"));
+		Assert.assertNotNull(loginButton);
 	}
 }
---
+++ archproject/src/test/java/com/bgasparotto/archproject/web/security/RegisterTest.java
@@ -0,0 +1,49 @@
+package com.bgasparotto.archproject.web.security;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.bgasparotto.archproject.web.ArquillianTestCase;
+
+public class RegisterTest extends ArquillianTestCase {
+
+	@Override
+	protected String targetUrl() {
+		return "localhost:8080/archproject";
+	}
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		By signUp = By.name("sign-up");
+		WebElement signUpLink = driver.findElement(signUp);
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.elementToBeClickable(signUp));
+		signUpLink.click();
+	}
+	
+	@Test
+	public void shouldFindSignUpElements() {
+		WebElement usernameInput = driver.findElement(By.name("username"));
+		Assert.assertNotNull(usernameInput);
+		
+		WebElement emailInput = driver.findElement(By.name("email"));
+		Assert.assertNotNull(emailInput);
+		
+		WebElement passwordInput = driver.findElement(By.name("password"));
+		Assert.assertNotNull(passwordInput);
+		
+		WebElement confirmPasswordInput = driver
+				.findElement(By.name("confirmPassword"));
+		Assert.assertNotNull(confirmPasswordInput);
+		
+		WebElement registerButton = driver
+				.findElement(By.name("registerButton"));
+		Assert.assertNotNull(registerButton);
+	}
+}
