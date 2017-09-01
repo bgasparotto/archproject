b238b4bf1f06b083b0ad35b146409fcca0b2eda3
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-09-01 14:06:31 -0300

    Reorganised the system roles.

--- archproject-dml.sql
+++ archproject-dml.sql
@@ -1,2 +1,2 @@
 -- Creates a default role 'LOGGED' on the database. Make sure to run archproject-ddl.sql before running the queries below
-INSERT INTO SECURITY.ROLE (NAME) VALUES ('LOGGED');
+INSERT INTO SECURITY.ROLE (NAME) VALUES ('USER'), ('GUEST'), ('ADMIN');
