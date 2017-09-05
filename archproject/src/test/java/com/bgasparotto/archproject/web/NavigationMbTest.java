aee8dc5e456aaed4dd514792fe1b96c0e1de3c15
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-09-05 12:54:00 -0300

    Added NavigationMb unit tests.

---
+++ archproject/src/test/java/com/bgasparotto/archproject/web/NavigationMbTest.java
@@ -0,0 +1,48 @@
+package com.bgasparotto.archproject.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NavigationMbTest {
+
+	@Test
+	public void shouldStartOnWelcomePage() throws Exception {
+		NavigationMb mb = new NavigationMb();
+		String page = mb.getPage();
+
+		Assert.assertEquals("welcome.xhtml", page);
+	}
+
+	@Test
+	public void shouldStartOnDefinedPage() throws Exception {
+		String somewhere = "somewhere.xhtml";
+		NavigationMb mb = new NavigationMb(somewhere);
+		String page = mb.getPage();
+
+		Assert.assertEquals(somewhere, page);
+	}
+
+	@Test
+	public void shouldNav() throws Exception {
+		NavigationMb mb = new NavigationMb();
+
+		String somewhereElse = "somewhere-else.xhtml";
+		mb.nav(somewhereElse);
+
+		String page = mb.getPage();
+		Assert.assertEquals(somewhereElse, page);
+	}
+
+	@Test
+	public void shouldSetAndGetAnotherPage() throws Exception {
+		String toBeSet = "to-be-set.xhtml";
+		NavigationMb mb = new NavigationMb();
+
+		String page = mb.getPage();
+		Assert.assertNotEquals(toBeSet, page);
+
+		mb.setPage(toBeSet);
+		page = mb.getPage();
+		Assert.assertEquals(toBeSet, page);
+	}
+}
