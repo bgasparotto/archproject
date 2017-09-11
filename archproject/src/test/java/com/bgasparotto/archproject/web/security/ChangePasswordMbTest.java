a2b85cbc42283b9e97f5aceef437c5cc9cea08fd
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-09-11 10:01:00 -0300

    Created missing unit tests for ChangePasswordMb.

--- archproject/src/test/java/com/bgasparotto/archproject/web/security/ChangePasswordMbTest.java
+++ archproject/src/test/java/com/bgasparotto/archproject/web/security/ChangePasswordMbTest.java
@@ -1,26 +1,34 @@
 package com.bgasparotto.archproject.web.security;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import com.bgasparotto.archproject.model.Login;
 import com.bgasparotto.archproject.model.Password;
+import com.bgasparotto.archproject.model.User;
+import com.bgasparotto.archproject.service.exception.ServiceException;
+import com.bgasparotto.archproject.service.usernrole.TestingUserFactory;
 import com.bgasparotto.archproject.service.usernrole.UserService;
 import com.bgasparotto.archproject.service.usernrole.UserServiceImpl;
+import com.bgasparotto.archproject.web.util.FacesMessenger;
+import com.bgasparotto.archproject.web.util.FacesUser;
 
 public class ChangePasswordMbTest {
 	private ChangePasswordMb mb;
 
 	@Before
 	public void setUp() throws Exception {
-		UserService mockService = mock(UserServiceImpl.class);
-		Logger logger = LoggerFactory.getLogger(ChangePasswordMb.class);
+		UserService service = mock(UserServiceImpl.class);
+		FacesMessenger messenger = mock(FacesMessenger.class);
+		FacesUser facesUser = mock(FacesUser.class);
 
-		mb = new ChangePasswordMb(mockService, logger);
+		mb = new ChangePasswordMb(service, messenger, facesUser);
 	}
 
 	@Test
@@ -34,7 +42,60 @@ public class ChangePasswordMbTest {
 		UserService userService = mb.getUserService();
 		Assert.assertNotNull(userService);
 
-		Logger logger = mb.getLogger();
-		Assert.assertNotNull(logger);
+		FacesMessenger messenger = mb.getMessenger();
+		Assert.assertNotNull(messenger);
+
+		FacesUser facesUser = mb.getFacesUser();
+		Assert.assertNotNull(facesUser);
+	}
+
+	@Test
+	public void shouldChangePasswordAndDisplayInfoMessage() throws Exception {
+		User user = TestingUserFactory.newUserInstance();
+		Login login = user.getLogin();
+		String username = login.getUsername();
+
+		FacesUser facesUser = mb.getFacesUser();
+		when(facesUser.getRemoteUser()).thenReturn(username);
+
+		UserService userService = mb.getUserService();
+		when(userService.changePassword(any(), any(), any())).thenReturn(user);
+
+		String navigation = mb.changePassword();
+		Assert.assertNull(navigation);
+		verify(facesUser).getRemoteUser();
+		FacesMessenger messenger = mb.getMessenger();
+		verify(messenger).info(any(), any());
+	}
+
+	@Test
+	public void shouldNotChangePasswordAndDisplayErrorMessage()
+			throws Exception {
+		FacesUser facesUser = mb.getFacesUser();
+		when(facesUser.getRemoteUser()).thenReturn("anyuser");
+
+		UserService userService = mb.getUserService();
+		when(userService.changePassword(any(), any(), any())).thenReturn(null);
+
+		String navigation = mb.changePassword();
+		Assert.assertNull(navigation);
+		verify(facesUser).getRemoteUser();
+		FacesMessenger messenger = mb.getMessenger();
+		verify(messenger).error(any(), any());
+	}
+
+	@Test
+	public void shouldFailToChangePasswordAndReturnError()
+			throws Exception {
+		FacesUser facesUser = mb.getFacesUser();
+		when(facesUser.getRemoteUser()).thenReturn("anyuser");
+
+		UserService userService = mb.getUserService();
+		when(userService.changePassword(any(), any(), any()))
+				.thenThrow(new ServiceException());
+
+		String navigation = mb.changePassword();
+		Assert.assertEquals("error", navigation);
+		verify(facesUser).getRemoteUser();
 	}
 }
