2649cf1ff37b4871de011c779185831bf0d675fe
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-09-15 10:02:35 -0300

    Updated the initial DML to insert an admin/admin user with ADMIN role.

--- archproject-dml.sql
+++ archproject-dml.sql
@@ -1,2 +1,18 @@
--- Creates a default role 'LOGGED' on the database. Make sure to run archproject-ddl.sql before running the queries below
+-- Make sure to run archproject-ddl.sql before running the queries below.
+-- The following queries assume you're running it on a fresh created database
+-- with the serial column values starting at 1 as Postgres' default.
+
+-- Creates the basic roles on the database. 
 INSERT INTO SECURITY.ROLE (NAME) VALUES ('USER'), ('GUEST'), ('ADMIN');
+
+-- Creates the 'admin' user with 'admin' password's value encrypted by BCrypt.
+INSERT INTO SECURITY.USER
+	(USERNAME, PASSWORD, EMAIL, REGISTRATION_DATE)
+VALUES
+	('admin', 
+	 '$2a$10$M8dgvnbTE8m5gdB0wC7m2uScIMYlNVo613Iilyjv8tNc./GlFz1XO', 
+	 'admin@domain.com', 
+	 '2017-09-15 00:00:00');
+
+-- Associates the admin user to the ADMIN role.
+INSERT INTO SECURITY.USER_ROLE (ID_USER, ID_ROLE) VALUES (1, 3);
