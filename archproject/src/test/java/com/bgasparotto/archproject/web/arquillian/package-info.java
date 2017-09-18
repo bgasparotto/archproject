846922c6558cf9b9a3a06f4f94b151d504e74b4a
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-09-18 20:29:59 -0300

    Added a couple package-info files to better explain the news packages.

---
+++ archproject/src/test/java/com/bgasparotto/archproject/web/arquillian/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing the ArquillianTestCase and any dependencies, if any. Also,
+ * sub-packages may be created to implement unit tests that need a container
+ * managed by Arquillian.
+ */
+package com.bgasparotto.archproject.web.arquillian;
---
+++ archproject/src/test/java/com/bgasparotto/archproject/web/arquillian/selenium/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing a SeleniumTestCase that relies on ArquillianTestCase in
+ * order to bring the container ready-to-use for unit tests.
+ */
+package com.bgasparotto.archproject.web.arquillian.selenium;
